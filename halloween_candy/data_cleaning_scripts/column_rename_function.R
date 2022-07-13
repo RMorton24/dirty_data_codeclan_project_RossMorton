404c272c03f96954d2d7ab38c1297d5553ac9d31
RMorton24 <ross.morton34@gmail.com>
2022-07-13 23:38:26 +0100

    Update of cleaning script- task 4
    * Updated task 4 cleaning script

--- halloween_candy/data_cleaning_scripts/column_rename_function.R
+++ halloween_candy/data_cleaning_scripts/column_rename_function.R
@@ -17,19 +17,40 @@
 #' 
 column_name_replace <- function(.data, old_names, new_names){
   
-  # Determine the location of names to replace which match vector check
-  old_index <- str_which(names(.data), str_c(old_names, collapse = "|"))
+  # # Determine the location of names to replace which match vector check
+  # old_index <- str_which(names(.data), str_c(old_names, collapse = "|"))
+  # 
+  # # Extract an array of terms which have been used
+  # string_used <- na.omit(str_extract(names(.data), 
+  #                                    str_c(old_names, collapse = "|")))
+  # 
+  # # Obtain the index of names which are to be used for replacement
+  # new_index <- str_which(old_names, 
+  #                        str_c(string_used, collapse = "|"))
+  # 
+  # # Change the variable names
+  # .data %>% 
+  #   rename_with(~ new_names[new_index], .cols = all_of(old_index))
   
-  # Extract an array of terms which have been used
-  string_used <- na.omit(str_extract(names(.data), 
-                                     str_c(old_names, collapse = "|")))
+
   
-  # Obtain the index of names which are to be used for replacement
-  new_index <- str_which(old_names, 
-                         str_c(string_used, collapse = "|"))
   
-  # Change the variable names
-  .data %>% 
-    rename_with(~ new_names[new_index], .cols = all_of(old_index))
-  
+  for (name_index in 1:length(old_names)){
+
+    if (any(str_detect(names(.data), old_names[name_index]))){
+      
+      if (sum(new_vector[name_index] == names(.data)) > 1){
+        stop(paste("Column name", new_vector[name_index], "already exists"))
+      }
+      
+      
+      names(.data)[str_which(names(.data), old_vector[name_index])] <- 
+        new_vector[name_index]
+    }
+  }
+  return(.data)
 }
+
+
+# hwc_2017 <- hwc_2017 %>% 
+#   rename_with(~new_name[which(str_detect(.x, old_names))], .cols = contains(old_names))
--- halloween_candy/data_cleaning_scripts/halloween_candy_clean_script.R
+++ halloween_candy/data_cleaning_scripts/halloween_candy_clean_script.R
@@ -13,7 +13,7 @@
 ## Date Created: 2022/07/01
 ## 
 ## Output:
-##  halloween_candy_clean.csv : 
+##  halloween_candy_clean.rds : 
 ##      Cleaned data of .csv file.
 ##      - 
 ## 
@@ -39,6 +39,7 @@
 library(tidyverse)
 library(readxl)
 library(janitor)
+library(assertr)
 
 
 # Load in functions -------------------------------------------------------
@@ -48,59 +49,98 @@ source(here::here("data_cleaning_scripts/column_rename_function.R"))
 
 
 # Read in data ------------------------------------------------------------
+# Note that some columns will be re
+hwc_2015 <- read_xlsx(here::here("raw_data/boing-boing-candy-2015.xlsx"))
 
-hwc_2015 <- read_xlsx(here::here("raw_data/boing-boing-candy-2015.xlsx")) %>% 
-  clean_names()
-hwc_2016 <- read_xlsx(here::here("raw_data/boing-boing-candy-2016.xlsx")) %>% 
-  clean_names()
-hwc_2017 <- read_xlsx(here::here("raw_data/boing-boing-candy-2017.xlsx")) %>% 
-  rename_with(~str_remove(.x, "^Q[:digit:]")) %>% 
-  clean_names()
+hwc_2016 <- read_xlsx(here::here("raw_data/boing-boing-candy-2016.xlsx"))
+
+hwc_2017 <- read_xlsx(here::here("raw_data/boing-boing-candy-2017.xlsx"))
 
 
 
-# Clean data --------------------------------------------------------------
+
+# Clean Column names------------------------------------------------------------
 
 # Search string - uses regex input
-old_vector <- c("how_old", "trick|going_out", "country", "state_prov", "_gender")
+old_vector <- c("how_old", "trick|going_out", "country", "state_prov", "_gender",
+                "brown_globs", "third_party", "bonkers", "raisins",
+                "kissa", "late_hersh", "^l.*y.*k",
+                "sweetums", "internal_id")
 # output name (!must match the length of )
-new_vector <- c("age", "trick_or_treat", "country", "region", "gender")
+new_vector <- c("age", "trick_or_treat", "country", "region", "gender",
+                "annonym_brown_globs", "independent_m_ms", "bonkers", "raisins",
+                "hersheys_kisses", "hersheys_dark_chocolate", "licorice",
+                "sweetums", "id_num")
 
 hwc_2015 <- hwc_2015 %>% 
+  select(2:3 , starts_with("[")) %>% 
+  clean_names() %>% 
   column_name_replace(old_vector, new_vector) %>% 
-  select(-starts_with(c("please","fill", "that_dress", "if_you","guess")))
+  rowid_to_column("id_num") %>% 
+  mutate(id_num = as.numeric(paste0("2015",str_pad(id_num, 5, pad = "0")))) %>% 
+  mutate(data_year = 2015L, .after = id_num)
+  #select(-starts_with(c("please","fill", "that_dress", "if_you","guess", "check")))
 
 hwc_2016 <- hwc_2016 %>% 
-  column_name_replace(old_vector, new_vector)
+  select(2:6 , starts_with("["), -ends_with("Ignore"), 
+         -contains(c("DVD", "board"))) %>%
+  clean_names() %>% 
+  column_name_replace(old_vector, new_vector) %>% 
+  rowid_to_column("id_num") %>% 
+  mutate(data_year = 2017L, .after = id_num) %>% 
+  mutate(id_num = as.numeric(paste0("2016",str_pad(id_num, 5, pad = "0")))) %>% 
+  mutate(data_year = 2016L, .after = id_num)
 
-hwc_2017 <- hwc_2017 %>% 
-  column_name_replace(old_vector, new_vector)
+hwc_2017 <- hwc_2017 %>%
+  select(1:6 , starts_with("Q6"), -contains(c("housewives", "board"))) %>%
+  rename_with(~str_remove(.x, "^Q[:digit:]")) %>% 
+  clean_names() %>% 
+  column_name_replace(old_vector, new_vector) %>% 
+  mutate(id_num = as.numeric(paste0("2017", 
+                                    str_pad(row_number(), 5, pad = "0")))) %>% 
+  mutate(data_year = 2017L, .after = id_num)
 
 
+
+# Combine data sets -------------------------------------------------------
+
+candy <- bind_rows(hwc_2017, hwc_2016, hwc_2015)
+
+
+# Clean Row data ----------------------------------------------------------
   
+candy <- candy %>% 
+  # Extract the numbers from the character array (will cause NA's for missing)
+  mutate(age = str_extract(age, "[:digit:]{1,3}[\\.[:digit:]]{0,5}")) %>% 
+  # If age is below 5 then set to an error
+  mutate(age = if_else(as.numeric(age) < 5, NA_real_, as.numeric(age))) %>% 
+  mutate(country = str_to_title(str_remove_all(country, "[:punct:]|(?i)the "))) %>% 
+  # Correct the country names
+  mutate(country = case_when(
+    # Remove numeric and set errors if required
+    !is.na(as.numeric(country)) | str_detect(country, "^Not") ~ NA_character_,
+    # Check for forms of United States
+    str_detect(country, 
+               paste0("(?i)[esd] s[ta]|(?<![Aa])(?i)u[ sd]{1,2}|(?i)m[ue]r{1,2}",
+                      "|Tru|y{4}$|(?i)w [yj]|(?i)ca[rl][oi]|(?i)pit|(?i)^Ala")) 
+               ~ "United States",
+    # Check for forms of United Kingdom
+    str_detect(country, "(?i)uk|d k|^(?i)En[dg]|(?i)Scot") ~ "United Kingdom",
+    # Check for Canada
+    str_detect(country, "(?i)can") ~ "Canada",
+    # Check for spain
+    country == "España" ~ "Spain",
+    # Otherwise set to current country
+    TRUE ~ country
+  )) %>% 
+  # Convert to pivot for easier analysis
+  # NOTE THAT THIS WILL DUPLICATE THE FIRST FEW ROWS BEFORE "REGION"
+  pivot_longer((str_which(names(candy), "region")+1):last_col(), 
+                 names_to = "sweet_name", values_to = "thoughts")
 
-# column_name_replace <- function(.data, old_names, new_names){
-#   
-#   # Determine the location of names to replace which match vector check
-#   old_index <- str_which(names(.data), str_c(old_names, collapse = "|"))
-#   
-#   # Extract an array of terms which have been used
-#   string_used <- na.omit(str_extract(names(.data), 
-#                                      str_c(old_names, collapse = "|")))
-#   
-#   # Obtain the index of names which are to be used for replacement
-#   new_index <- str_which(old_names, 
-#                          str_c(string_used, collapse = "|"))
-#   
-#   # Change the variable names
-#   .data %>% 
-#     rename_with(~ new_names[new_index], .cols = all_of(old_index))
-#   
-# }
 
-
-
-
+# Write Output -------------------------------------------------------------
+write_rds(candy, here::here("clean_data/halloween_candy_clean.rds"))
 
 
 
@@ -108,11 +148,11 @@ hwc_2017 <- hwc_2017 %>%
 # 
 # names(hwc_2015)[which(!(names(hwc_2015) %in% names(hwc_2016)))]
 # 
- names(hwc_2016)[which(!(names(hwc_2016) %in% names(hwc_2017)))]
+# names(hwc_2016)[which(!(names(hwc_2016) %in% names(hwc_2017)))]
+# # 
+# names(hwc_2015)[which(!(names(hwc_2015) %in% names(hwc_2017)))]
 # 
-names(hwc_2015)[which(!(names(hwc_2015) %in% names(hwc_2017)))]
-
-
+# names(hwc_2017)[which(!(names(hwc_2017) %in% names(hwc_2016)))]
 
 # What is the total number of candy ratings given across the three years. 
 # (Number of candy ratings, not the number of raters. Don’t count missing values)
@@ -137,3 +177,27 @@ names(hwc_2015)[which(!(names(hwc_2015) %in% names(hwc_2017)))]
 # 
 # hwc_2017 <- hwc_2017 %>% 
 #   rename_with(~new_name[which(str_detect(.x, old_names))], .cols = contains(old_names))
+
+
+
+
+
+
+# column_name_replace <- function(.data, old_names, new_names){
+#   
+#   # Determine the location of names to replace which match vector check
+#   old_index <- str_which(names(.data), str_c(old_names, collapse = "|"))
+#   
+#   # Extract an array of terms which have been used
+#   string_used <- na.omit(str_extract(names(.data), 
+#                                      str_c(old_names, collapse = "|")))
+#   
+#   # Obtain the index of names which are to be used for replacement
+#   new_index <- str_which(old_names, 
+#                          str_c(string_used, collapse = "|"))
+#   
+#   # Change the variable names
+#   .data %>% 
+#     rename_with(~ new_names[new_index], .cols = all_of(old_index))
+#   
+# }
